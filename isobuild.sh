541292d8a49e0192aaafbf53a4c24c2d31a4b749
Hurng-Chun Lee <h.lee@donders.ru.nl>
2026-07-10 20:24:44 +0200

    provision bootc-users group

--- isobuild.sh
+++ isobuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo podman run --network=host --rm -it --privileged --pull=newer --security-opt label=type:unconfined_t -v ./isobuild.toml:/config.toml:ro -v ./output:/output -v /var/lib/containers/storage:/var/lib/containers/storage ghcr.io/osbuild/bootc-image-builder:latest -v --log-level debug --type anaconda-iso --rootfs xfs ghcr.io/hurngchunlee/laptop-fedora-gnome:44
+sudo podman run --network=host --rm -it --privileged --pull=newer --security-opt label=type:unconfined_t -v ./isobuild.toml:/config.toml:ro -v ./output:/output -v /var/lib/containers/storage:/var/lib/containers/storage ghcr.io/osbuild/bootc-image-builder:latest -v --log-level debug --type anaconda-iso --rootfs xfs ghcr.io/hurngchunlee/fedora-silverblue:44
