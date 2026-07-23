1b548cc7688530fe847a72ed59bc51a5dcb98299
Hurng-Chun Lee <h.lee@donders.ru.nl>
2026-07-23 15:14:46 +0200

    first working kinoite build, update isobuild script

--- isobuild.sh
+++ isobuild.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-image=ghcr.io/dccn-tg/fedora-silverblue:latest
+images=(fedora-silverblue fedora-kinoite) 
 
-sudo podman run \
-    --network=host \
-    --rm -it --privileged \
-    --pull=newer \
-    --security-opt label=type:unconfined_t \
-    -v ./blueprint.toml:/config.toml:ro \
-    -v ./output:/output \
-    -v /var/lib/containers/storage:/var/lib/containers/storage \
-    ghcr.io/osbuild/bootc-image-builder:latest \
-      -v --log-level debug \
-      --type anaconda-iso \
-      --rootfs xfs \
-      $image
+for image in ${images[@]}; do
+    podman pull ghcr.io/dccn-tg/${image}:latest &&
+    podman run \
+        --network=host \
+        --rm -it --privileged \
+        --pull=newer \
+        --security-opt label=type:unconfined_t \
+        -v ./blueprint.toml:/config.toml:ro \
+        -v ./output:/output \
+        -v /var/lib/containers/storage:/var/lib/containers/storage \
+        ghcr.io/osbuild/bootc-image-builder:latest \
+          -v --log-level debug \
+          --type anaconda-iso \
+          --rootfs xfs \
+          ghcr.io/dccn-tg/${image}:latest &&
+    mv ./output/bootiso/install.iso ./output/bootiso/${image}.iso
+done
